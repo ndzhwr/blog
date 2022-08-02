471c7ada7daca7354d693e7dfcd958cca19e50f9
rexisn <ndizihiweregis06@gmail.com>
2022-08-02 08:51:33 +0200

    Making  blog not effective
    revert: changed the method of addding a blog  manually not using markdown

--- client/src/pages/NewBlog.tsx
+++ client/src/pages/NewBlog.tsx
@@ -11,8 +11,9 @@ const NewBlog: React.FC = () => {
     const [blogCategory, setBlogCategory] = React.useState('')
     const [blogImage, setblogImage] = React.useState<File>()
     const [categories, setCategories] = React.useState<any[]>([])
-
-    const formData = new FormData()
+    const createBlogForm  = document.querySelector('form')
+    const formData : FormData = new FormData(createBlogForm as HTMLFormElement)
+    console.log(formData)
     async function fetchCategories() {
         const res = await axios.get('http://localhost:7777/categories')
         const CategoriesWithoutAll: any[] = res.data.filter((category: { name: string }) => category.name !== 'All')
@@ -33,19 +34,21 @@ const NewBlog: React.FC = () => {
             <div className="content  w-full grid grid-cols-4 ">
 
                 <div className="header col-start-1 border col-end-3 py-10  px-10  pb-4 overflow-y-scroll  scrollDiv bg-sky-100">
-                    <input type="file" placeholder='Blog description picture' onChange={(e) => e.target.files && e.target.files.length != 0 && setblogImage(e.target.files[0])} />
-                    <div className="flex items-center">
+                    <form id='createBlogForm'>
+                        <input type="file" placeholder='Blog description picture' onChange={(e) => e.target.files && e.target.files.length != 0 && setblogImage(e.target.files[0])} />
+                        <div className="flex items-center">
 
-                        <input type="text" className=' border-gray-300 border px-4 py-2  w-full mb-2 text-sm rounded-md ' placeholder='Blog title' onChange={(e) => setBlogTitle(e.target.value)} />
-                        <select name="category" className=' px-4    border text-sm  border-gray-300 rounded-md mb-2 py-2' placeholder='Select a category' onChange={(e) => setBlogCategory(e.target.value)}>
-                            {categories.map(category => (
-                                <option value={category.name} className='px-4 py-2 border '>{category.name}</option>
-                            ))}
-                        </select>
-                    </div>
-                    <textarea className='w-full no-resize border border-gray-300 h-[100px] px-4 py-2 text-sm rounded-md ' placeholder='Blog description' onChange={(e) => setBlogDescription(e.target.value)} />
-                    <textarea className='w-full no-resize border border-gray-300 h-[500px] px-4 py-2 text-sm rounded-md ' placeholder='Blog content / markdown only' onChange={(e) => setContent(e.target.value)} />
-                    <button className='text-sm w-full bg-sky-500 py-2 text-white rounded-md'>Create blog</button>
+                            <input type="text" className=' border-gray-300 border px-4 py-2  w-full mb-2 text-sm rounded-md ' placeholder='Blog title' name='blog_title' onChange={(e) => setBlogTitle(e.target.value)} />
+                            <select className=' px-4    border text-sm  border-gray-300 rounded-md mb-2 py-2' placeholder='Select a category' name='blog_category' onChange={(e) => setBlogCategory(e.target.value)}>
+                                {categories.map(category => (
+                                    <option value={category.name} className='px-4 py-2 border '>{category.name}</option>
+                                ))}
+                            </select>
+                        </div>
+                        <textarea name="blog_description" className='w-full no-resize border border-gray-300 h-[100px] px-4 py-2 text-sm rounded-md ' placeholder='Blog description' onChange={(e) => setBlogDescription(e.target.value)} />
+                        <textarea name='blog-content' className='w-full no-resize border border-gray-300 h-[500px] px-4 py-2 text-sm rounded-md ' placeholder='Blog content / markdown only' onChange={(e) => setContent(e.target.value)} />
+                        <button className='text-sm w-full bg-sky-500 py-2 text-white rounded-md'>Create blog</button>
+                    </form>
 
                 </div>
                 <div className="col-start-3 border col-end-5  py-10 px-4 overflow-y-scroll scrollDiv hidden md:block" >
@@ -58,6 +61,7 @@ const NewBlog: React.FC = () => {
                         </div>
                     )}
                     <div id="previewer">
+                        <img src={''} />
                         <h1 className='font-bold text-xl mb-2'>{blogTitle}</h1>
                         <p className=' text-right text-[#c4c4c4]'>{blogCategory}</p>
                         <p className=' text-sm text-[#c4c4c4] mb-4'>{blogDescription}</p>
